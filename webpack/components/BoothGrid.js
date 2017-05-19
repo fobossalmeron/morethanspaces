cd222eaa1b575629ef022eaa6966931f935dd183
fobossalmeron <fobos.salmeron@gmail.com>
2017-05-19 16:36:13 -0500

    database retreiving boothgrid

--- webpack/components/BoothGrid.js
+++ webpack/components/BoothGrid.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
-import './db.js';
+//import './../db/db.json';
 
 class BoothGrid extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+    this.loadFromServer = this.loadFromServer.bind(this);
+  }
+
+  loadFromServer () {
+       var xhr = new XMLHttpRequest();
+       xhr.open('get', './assets/js/booths2.json', true);
+       xhr.onload = function() {
+           var data = JSON.parse(xhr.responseText);
+           this.setState({ data: data.booths });
+
+       }.bind(this);
+       xhr.send();
+   }
+
+   componentDidMount() {
+    this.loadFromServer();
+}
+
   render (){
+    var allBooths = this.state.data.map((booth, index) => (
+      <li key={booth.id} className={"boothGridItem booth" + booth.type}>
+            <img src={booth.images[1]}/>
+            <label>{booth.id}</label>
+      </li>
+     ));
     return (
       <ul id="boothGrid">
-          <div className="boothGridItem"></div>
-        
+          {allBooths}
       </ul>
     );
   }

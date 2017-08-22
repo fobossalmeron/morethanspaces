7d8b493daca691e9708502e33551cc23520e751d
fobossalmeron <fobos.salmeron@gmail.com>
2017-08-22 15:42:11 -0500

    discount file

---
+++ assets/js/discount.js
@@ -0,0 +1,19 @@
+// Morethanspaces discount file.
+
+// write the amount of dollars or the percentage of the discount.
+// ej:
+// var discountNumber = 25      (for a percentage)
+// var discountNumber = 2500    (for an ammount)
+// don't use symbols or currencies.
+
+var discountNumber = 25
+
+// if this is set to false, discount will be treated as the
+// amount of dollars substracted from the final quote.
+// if this is set to true, discount will be treated as the
+// percentage number.
+
+var isDiscountAPercentage = true
+
+// write the text you wan't displayed, use single quotes ' '.
+var discountText = 'off your next Quote!'
--- webpack/components/ContactSection.js
+++ webpack/components/ContactSection.js
@@ -1,82 +1,6 @@
 import React, { Component } from 'react';
 import { Form, Text, Select, Textarea, NestedForm, FormError } from 'react-form'
 
-const contactForm = (
-  <Form
-    onSubmit={(values) => {
-      console.log('Form Submitted Succesfully with:', values)
-
-      const url = 'https://formspree.io/fobos.salmeron@gmail.com';
-      var data = values;
-
-      var xhr = new XMLHttpRequest();
-          xhr.open('POST', url, true);
-          xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
-          xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-
-          // Send the collected data as JSON
-          xhr.send(JSON.stringify(data));
-
-          // Callback function
-          xhr.onloadend = function (response) {
-              if (response.target.status === 0) {
-                  // Failed XmlHttpRequest should be considered an undefined error.
-                  console.log('danger');
-
-              } else if (response.target.status === 400) {
-                  console.log(JSON.parse(responseText).error);
-
-              } else if (response.target.status === 200) {
-                  console.log('Success!')
-
-              }
-          }
-       }
-    }
-
-    validate={({ name, email, business, message }) => {
-      return {
-        name: !name ? 'A name is required' : undefined,
-        business: !business ? 'A business name is required' : undefined,
-        message: !message? 'The message cannot be empty' : undefined,
-        email:
-          !email ?
-        'The email cannot be empty' :
-          email.search('@') == -1?
-        'Please give a valid email' :
-          email.search(/gmail.com/i) !== -1?
-        'Sorry for the inconvenience but we only work with businesses, please provide a business email' :
-          email.search(/aol.com/i) !== -1?
-        'Sorry for the inconvenience but we only work with businesses, please provide a business email' :
-          email.search(/yahoo.com/i) !== -1?
-        'Sorry for the inconvenience but we only work with businesses, please provide a business email' :
-          email.search(/live.com/i) !== -1?
-        'Sorry for the inconvenience but we only work with businesses, please provide a business email' :
-          email.search(/hotmail.com/i) !== -1?
-        'Sorry for the inconvenience but we only work with businesses, please provide a business email' :
-          email.search('@') == -1?
-        'Please give a valid email' :
-          undefined
-      }
-    }}
-    >
-    {({submitForm}) => {
-      return (
-        <form onSubmit={submitForm}>
-          <Text field='name' placeholder='your name'/>
-          <Text field='email' placeholder='your email'/>
-          <Text field='business' placeholder='your business name'/>
-          <Textarea
-            field='message'
-            placeholder='message'
-          />
-          <button type='submit'>Send</button>
-        </form>
-      )
-    }}
-  </Form>
-)
-
 class ContactSection extends Component {
   constructor(props) {
     super(props);
@@ -89,10 +13,12 @@ class ContactSection extends Component {
     console.log("Success from function");
     this.setState({
         successMessage: true
-    });
+    })
   }
   render (){
     var calendlyUrl = 'https://calendly.com/morethanspaces'
+    const message = this.state.successMessage ? 'THANKS!' : 'send us an email';
+    const messageClass = this.state.successMessage ? 'half-card messaged' : 'half-card';
     return (
       <section id="contact">
         <h2><b>contact</b></h2>
@@ -101,9 +27,82 @@ class ContactSection extends Component {
         will be more than happy to answer any of your questions. Please fill out your information below with
         your question and we will be happy to answer you back or if you need immediate assistant please give
         us a call at 1833-morethanspaces.</p>
-        <div className="half-card">
-          <h3>send us an email</h3>
-          {contactForm}
+        <div className={messageClass}>
+          <h3>{message}</h3>
+          <Form
+            onSubmit={(values) => {
+              console.log('Form Submitted Succesfully with:', values)
+
+              const url = 'https://formspree.io/fobos.salmeron@gmail.com';
+              var data = values;
+
+              var xhr = new XMLHttpRequest();
+                  xhr.open('POST', url, true);
+                  xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
+                  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
+                  // Send the collected data as JSON
+                  xhr.send(JSON.stringify(data));
+
+                  // Callback function
+                  xhr.onloadend = function (response) {
+                      if (response.target.status === 0) {
+                          // Failed XmlHttpRequest should be considered an undefined error.
+                          console.log('danger');
+
+                      } else if (response.target.status === 400) {
+                          console.log(JSON.parse(responseText).error);
+
+                      } else if (response.target.status === 200) {
+                          console.log('Success!');
+                          () => this.showConf();
+
+                      }
+                  }
+               }
+            }
+
+            validate={({ name, email, business, message }) => {
+              return {
+                name: !name ? 'A name is required' : undefined,
+                business: !business ? 'A business name is required' : undefined,
+                message: !message? 'The message cannot be empty' : undefined,
+                email:
+                  !email ?
+                'The email cannot be empty' :
+                  email.search('@') == -1?
+                'Please give a valid email' :
+                  email.search(/gmail.com/i) !== -1?
+                'Sorry for the inconvenience but we only work with businesses, please provide a business email' :
+                  email.search(/aol.com/i) !== -1?
+                'Sorry for the inconvenience but we only work with businesses, please provide a business email' :
+                  email.search(/yahoo.com/i) !== -1?
+                'Sorry for the inconvenience but we only work with businesses, please provide a business email' :
+                  email.search(/live.com/i) !== -1?
+                'Sorry for the inconvenience but we only work with businesses, please provide a business email' :
+                  email.search(/hotmail.com/i) !== -1?
+                'Sorry for the inconvenience but we only work with businesses, please provide a business email' :
+                  email.search('@') == -1?
+                'Please give a valid email' :
+                  undefined
+              }
+            }}
+            >
+            {({submitForm}) => {
+              return (
+                <form onSubmit={submitForm}>
+                  <Text field='name' placeholder='your name'/>
+                  <Text field='email' placeholder='your email'/>
+                  <Text field='business' placeholder='your business name'/>
+                  <Textarea
+                    field='message'
+                    placeholder='message'
+                  />
+                  <button type='submit'>Send</button>
+                </form>
+              )
+            }}
+          </Form>
         </div>
         <div className="half-card">
           <h3>or schedule a call!</h3>

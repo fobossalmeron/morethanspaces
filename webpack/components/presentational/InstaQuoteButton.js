9a2f83be6013d8abe3299f0a2c5c18faa1b0e6a8
fobossalmeron <fobos.salmeron@gmail.com>
2017-10-30 18:30:40 -0600

    insta button

--- webpack/components/presentational/InstaQuoteButton.js
+++ webpack/components/presentational/InstaQuoteButton.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import InstaQuoteIcon from 'svg-react-loader?name=InstaQuoteIcon!../../../assets/img/layout/instaQuote.svg';
 
 class InstaQuoteButton extends Component {
+  constructor(props) {
+    super(props);
+    this.scrollToProducts = this.scrollToProducts.bind(this);
+  }
+
+  scrollToProducts(){
+    controller.scrollTo("#products");
+    if (window.history && window.history.pushState) {
+        history.pushState("", document.title, '#products');
+    }
+  }
 
   render (){
     return (
-      <a href="#products">
-        <div className={"bounce"} id="instaQuoteButton"><InstaQuoteIcon/></div>
-      </a>
+        <div onClick={this.scrollToProducts} className={"bounce"} id="instaQuoteButton"><InstaQuoteIcon/></div>
     );
   }
 };

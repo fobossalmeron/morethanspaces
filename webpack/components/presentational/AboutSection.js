3e6f0990922007adcfd1bdce8df9beb635357bcb
fobossalmeron <fobos.salmeron@gmail.com>
2018-02-08 22:56:40 -0600

    edit about section

--- webpack/components/presentational/AboutSection.js
+++ webpack/components/presentational/AboutSection.js
@@ -23,16 +23,12 @@ class StaticSection extends Component {
           <div className="half-card frompdf">
             <span>1,</span> <span>2,</span> <span>3 </span> <span> go</span>
             <p>All you have to do is show up and <b>let the show begin!</b></p>
-            <div id="stepbox1" className="stepbox"><b>1:</b><p>communicate your<br/>need and choose</p></div>
+            <div id="stepbox1" className="stepbox"><b>1:</b><p>communicate your need<br/>free 3D renders and animation<br/>select and done</p></div>
             <div id="stepbox2" className="stepbox"><b>2:</b><p>we create and<br/>make it happen</p></div>
             <div id="stepbox3" className="stepbox"><b>3:</b><p>we deliver<br/>…and take care of it all</p></div>
-            <div className="stepblackbox"><b className="arbitraryPink">best price, best quality</b><p>we try to <b>match or beat any price</b> up to <b>20%</b></p></div>
+            <div className="stepblackbox"><b className="arbitraryPink">best price, best quality</b></div>
           </div>
           <div className="half-card">
-            <h2 className="motto">
-              <b>we don’t believe in pressure sales,<br/>
-              schedule with confidence</b>
-            </h2>
             <div className="stepyellowbox">
               <p><b>more for less</b><br/>better service, better products, <br/>better results, less stress, less worry</p>
             </div>
